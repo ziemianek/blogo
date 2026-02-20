a747389d234b3579868f082058d9eb8c79ed5b5f
Michal Ziemianek <mziemianek3@gmail.com>
2026-02-20 12:39:17 +0100

    feat(core): add basic template rendering

--- cmd/blogo/main.go
+++ cmd/blogo/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"log"
+	"os"
+)
+
+const Title string = "Test strona"
+
+type config struct {
+	Title string
+}
 
 func main() {
-	fmt.Println("Hello, World")
+	check := func(err error) {
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	file, err := os.Create("output.html")
+	check(err)
+	defer file.Close()
+
+	tpl, err := template.ParseFiles("./web/static/html/index.tmpl")
+	check(err)
+
+	tpl.Execute(file, &config{Title: Title})
+	check(err)
 }
